c41b4b0477297ce69ebda4af01f357c4171218f8
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-01 10:30:36 +0530

    index

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //index
-
-
-
+const { add , sub } =require('./math')
 
 // this is the normal export the module function of math but also exports this method!
-const math = require('./math');
+// const math = require('./math');
 
-console.log(math); //
+// console.log(math); //
 
-console.log(math.add(3, 4)) //In this function math function will be call the using add and substarct the function.
+// console.log(math.add(3, 4)) //In this function math function will be call the using add and substarct the function.
 
-
+// console.log(add(5,6));     // destructuring the function of add and sub.
+// console.log(sub(19,20));
 
 // let Hello = console.log("Hello There! i am js")
-
 
 // console.log(math);
 // console.log(math(3, 4))

4d83f80ac74278f4fb8b5922278a6c7da68f11d9
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-01 12:52:06 +0530

    file

---
+++ file.js
@@ -0,0 +1,52 @@
+ const fs = require("fs")
+
+// console.log(fs);
+// sync...
+
+// fs.writeFileSync("./test.txt", "Hello saurabh are you there")
+
+
+// Async...
+// fs.writeFile("./textfile.txt", "Hello this is a async file", (err) => {})
+
+
+// sync readthefile
+
+// const result = fs.readFileSync('./contact.txt', "utf-8");
+// console.log(result);
+
+
+// Asyncchronous file will be reading in this system
+
+// fs.readFile('./contact.txt', "utf-8", (err,result) =>{
+//     if(err){
+//         console.log("Error", err); 
+//     }
+//     else{
+//         console.log("Result will be", result);
+//     }
+// })
+
+// sync file append
+// fs.appendFileSync('./test.txt', new Date().getDate().toLocaleString())
+// fs.appendFileSync('./test.txt', `${Date.now()} Hey there\n`)
+
+// fs.cpSync('./test.txt',"./copy.txt");
+
+// fs.unlinkSync('./copy.txt')
+
+
+console.log(fs.statSync("./test.txt", ).isFile())
+
+
+fs.mkdirSync('my-docs/a/b', {recursive: true})
+
+
+
+
+
+
+
+
+
+

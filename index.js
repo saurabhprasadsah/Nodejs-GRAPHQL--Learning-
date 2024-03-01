e3ccc683f5d651ce9dca320c38b8200e1b86a8d0
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-01 10:20:36 +0530

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let Hello = console.log("Hello There! i am js")
+//index
+const math = require('./math');
+
+console.log(math); //
+
+console.log(math.add(3, 4)) //In this function math function will be call the using add and substarct the function.
 
 
-// console.log(Hello);  
+
+// let Hello = console.log("Hello There! i am js")
+
+
+// console.log(math);
+// console.log(math(3, 4))
+// console.log(Hello); 
 // console.log(window);
---
+++ math.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a+b;
+
+}
+
+function sub(a,b){
+    return a-b;
+
+}
+
+
+
+module.exports = add ,sub //when call this exports then only add will call
+
+
+//When you are convert the exports into the object then it will be convert the objects.
+
+module.exports = {
+    sub,
+    add
+}
